163db2240a5a86a15d1e482b719cae5604bee844
oussama seme elayne <semeoussama@gmail.com>
2021-09-21 16:44:52 +0100

    adding multi way variant chips

--- project01/Not.c
+++ project01/Not.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-  
+
 int main()
 {
-  
-    int a[5] = { 1, 0, 1, 0, 1 };
+
+    int a[2] = { 1, 0};
     int i, ans;
-  
-    for (i = 0; i < 5; i++) {
+
+    for (i = 0; i < 2; i++) {
         if (a[i] == 0)
             ans = 1;
         else
